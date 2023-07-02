ee7585fe1ab7b5c2ebe7f1b2f6d1be67631b2664
Uzi <gssdsudas@gmail.com>
2023-07-02 12:53:56 +0400

    Bug fixes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ export default function App() {
   const name = useRef(null)
   const task = useRef(null)
   function createTodo() {
+    let inputs = [document.querySelector('input'), document.querySelector('textarea')]
+    Array.from(inputs).map(el=>el.value='')
     let key = Math.random()*1000
     localStorage.setItem(key, `${name.current}${key}${task.current}`)
     toUpdate(!Update)
@@ -14,8 +16,8 @@ export default function App() {
       <div className="todos">
         <div className="input">
           <div className="input__inputs">
-            <input onChange={e=>name.current = e.currentTarget.value} maxLength="25" type="text" className="input__inputs__name" placeholder="Название" />
-            <textarea onChange={e=>task.current = e.currentTarget.value} maxLength="230" className="input__inputs__task" placeholder="Задание"></textarea>
+            <input onChange={e=>{name.current = e.currentTarget.value}} maxLength="25" type="text" className="input__inputs__name" placeholder="Название" />
+            <textarea onChange={e=>{task.current = e.currentTarget.value}} maxLength="230" className="input__inputs__task" placeholder="Задание"></textarea>
           </div>
           <div className='input__buttonContent'>
             <button onClick={createTodo} className="input__create"><img className="input__create__img" src="./add.png"/></button>
